2eef7a634af2b3a0596069e4f2d056bc57720212
jacobkrol <33527722+jacobkrol@users.noreply.github.com>
2023-07-23 22:12:38 +0000

    Deploying to master from @ jacobkrol/jacobkrol.github.io@43cdbf44a48baf37c002031e4cb1419acceec4f5 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c0793c635851242428b10440c493a772.js"
+  "/precache-manifest.54bb5960a9db9a742567fa845ae75b77.js"
 );
 
 self.addEventListener('message', (event) => {
